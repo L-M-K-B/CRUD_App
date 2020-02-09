f8e6c9342862b8fa3fbfbc59806024fbad2379fc
L-M-K-B <laura.m.k.benz@gmail.com>
2020-02-09 15:15:50 +0100

    feat: add migration tool

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -7,17 +8,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://laura@localhost:5432/todoapp
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-
-db.create_all()
+# Do not use this anymore when using migrations
+# db.create_all()
 
 # HTML form submission
 # @app.route('/todos/create', methods=['POST'])
