07a79cb6785b5c0fddcb114068de899212f19a0d
L-M-K-B <laura.m.k.benz@gmail.com>
2020-02-15 15:20:40 +0100

    feat: complete practices

--- app.py
+++ app.py
@@ -21,15 +21,17 @@ class Todo(db.Model):
         'todolists.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description} {self.completed}>'
 
 
 class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     # lazy=True is the default setting anyway
-    todos = db.relationship('Todo', backref='list', lazy=True)
+    todos = db.relationship('Todo', backref='list',
+                            cascade="all, delete-orphan", lazy=True)
 
 # Do not use this anymore when using migrations
 # db.create_all()
@@ -44,17 +46,19 @@ class TodoList(db.Model):
 #     # url_for could also contain a second argument for data
 #     return redirect(url_for('index'))
 
-
+# create todos
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         todo_description = request.get_json()['description']
-        todo = Todo(description=todo_description)
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=todo_description, list_id=list_id)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
+        body['list_id'] = list_id
     except:
         error = True
         db.session.rollback()
@@ -66,7 +70,7 @@ def create_todo():
     else:
         return jsonify(body)
 
-
+# toggle todos completed/notcompleted
 @app.route('/todos/<todo_id>/setCompleted', methods=['POST'])
 def setCompletedTodo(todo_id):
     try:
@@ -81,7 +85,7 @@ def setCompletedTodo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
-
+# delete single todo
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def deleteTodo(todo_id):
     try:
@@ -94,7 +98,7 @@ def deleteTodo(todo_id):
         db.session.close()
     return jsonify({'success': True})
 
-
+# create list
 @app.route('/lists/create', methods=['POST'])
 def create_list():
     error = False
@@ -116,22 +120,52 @@ def create_list():
     else:
         return jsonify(body)
 
+# toggle lists completed/notcompleted (incl. all child items)
+@app.route('/lists/<list_id>/setCompleted', methods=['POST'])
+def setCompletedList(list_id):
+    try:
+        completed = request.get_json()['completed']
+        todolist = TodoList.query.get(list_id)
+        todolist.completed = completed
+        todos = Todo.query.filter(Todo.list_id == list_id).all()
+        for todo in todos:
+            todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
+# delete list
+@app.route('/lists/<list_id>', methods=['DELETE'])
+def deletelist(list_id):
+    try:
+        todolist = TodoList.query.get(list_id)
+        db.session.delete(todolist)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+# render lists
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template(
         'index.html',
         lists=TodoList.query.all(),
         active_list=TodoList.query.get(list_id),
-        todos=Todo.query.filter_by(list_id=list_id).all()
+        todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
 
-
+# import static files (e.g. js file) into index.html
 @app.route('/static/<path>')
 def send_static(path):
     return send_from_directory('static', path)
 
-
+# render homepage
 @app.route('/')
 def index():
     return redirect(url_for('get_list_todos', list_id=1))
--- static/script.js
+++ static/script.js
@@ -1,93 +1,137 @@
 // create a new list
-document.querySelector("#list-form").onsubmit = event => {
-  event.preventDefault();
-  fetch("/lists/create", {
-    method: "POST",
-    body: JSON.stringify({
-      name: document.querySelector('[name="name"]').value
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(jsonResponse => {
-      console.log(jsonResponse);
-      const listItem = document.createElement("li");
-      listItem.innerHTML = jsonResponse["name"];
-      document.querySelector("#lists").appendChild(listItem);
-      document.querySelector("#error").className = "hidden";
+document.querySelector('#list-form').onsubmit = event => {
+    event.preventDefault();
+    fetch('/lists/create', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: document.querySelector('[name="name"]').value,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
-    .catch(() => {
-      document.querySelector("#error").className = "";
-    });
-  document.querySelector('[name="name"]').value = "";
+        .then(response => response.json())
+        .then(jsonResponse => {
+            console.log(jsonResponse);
+            const listItem = document.createElement('li');
+            listItem.innerHTML = jsonResponse['name'];
+            document.querySelector('#lists').appendChild(listItem);
+            document.querySelector('#error').className = 'hidden';
+        })
+        .catch(() => {
+            document.querySelector('#error').className = '';
+        });
+    document.querySelector('[name="name"]').value = '';
 };
 
+// toggle list to checked / not checked (incl. all child items)
+const listCheckboxes = document.querySelectorAll('.listCheckCompleted');
+for (let i = 0; i < listCheckboxes.length; i++) {
+    const listCheckbox = listCheckboxes[i];
+    listCheckbox.onchange = event => {
+        const listId = event.target.dataset['id'];
+        fetch('/lists/' + listId + '/setCompleted', {
+            method: 'POST',
+            body: JSON.stringify({
+                completed: event.target.checked,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(() => {
+                document.querySelector('#error').className = 'hidden';
+            })
+            .catch(() => {
+                document.querySelector('#error').className = '';
+            });
+    };
+}
+
+// delete a list and all child items
+const listDeleteButtons = document.querySelectorAll('.deleteList');
+for (let i = 0; i < listDeleteButtons.length; i++) {
+    const listDeleteBtn = listDeleteButtons[i];
+    listDeleteBtn.onclick = event => {
+        const listId = event.target.dataset['id'];
+        fetch('/lists/' + listId, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                document.querySelector('#error').className = 'hidden';
+            })
+            .catch(() => {
+                document.querySelector('#error').className = '';
+            });
+    };
+}
+
 // toggle todos to checked / not checked
-const checkboxes = document.querySelectorAll(".checkCompleted");
+const checkboxes = document.querySelectorAll('.checkCompleted');
 for (let i = 0; i < checkboxes.length; i++) {
-  const checkbox = checkboxes[i];
-  checkbox.onchange = event => {
-    const todoId = event.target.dataset["id"];
-    fetch("/todos/" + todoId + "/setCompleted", {
-      method: "POST",
-      body: JSON.stringify({
-        completed: event.target.checked
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(() => {
-        document.querySelector("#error").className = "hidden";
-      })
-      .catch(() => {
-        document.querySelector("#error").className = "";
-      });
-  };
+    const checkbox = checkboxes[i];
+    checkbox.onchange = event => {
+        const todoId = event.target.dataset['id'];
+        fetch('/todos/' + todoId + '/setCompleted', {
+            method: 'POST',
+            body: JSON.stringify({
+                completed: event.target.checked,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(() => {
+                document.querySelector('#error').className = 'hidden';
+            })
+            .catch(() => {
+                document.querySelector('#error').className = '';
+            });
+    };
 }
 
 // delete single todos
-const deleteButtons = document.querySelectorAll(".deleteTodo");
-for (let i = 0; i < deleteButtons.length; i++) {
-  const deleteBtn = deleteButtons[i];
-  deleteBtn.onclick = event => {
-    const todoId = event.target.dataset["id"];
-    fetch("/todos/" + todoId, {
-      method: "DELETE"
-    })
-      .then(() => {
-        document.querySelector("#error").className = "hidden";
-      })
-      .catch(() => {
-        document.querySelector("#error").className = "";
-      });
-  };
+const todoDeleteButtons = document.querySelectorAll('.deleteTodo');
+for (let i = 0; i < todoDeleteButtons.length; i++) {
+    const todoDeleteBtn = todoDeleteButtons[i];
+    todoDeleteBtn.onclick = event => {
+        const todoId = event.target.dataset['id'];
+        fetch('/todos/' + todoId, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                document.querySelector('#error').className = 'hidden';
+                window.location.reload(true);
+            })
+            .catch(() => {
+                document.querySelector('#error').className = '';
+            });
+    };
 }
 
 // create a new todo
-document.querySelector("#todo-form").onsubmit = event => {
-  event.preventDefault();
-  fetch("/todos/create", {
-    method: "POST",
-    body: JSON.stringify({
-      description: document.querySelector('[name="description"]').value
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(jsonResponse => {
-      console.log(jsonResponse);
-      const listItem = document.createElement("li");
-      listItem.innerHTML = jsonResponse["description"];
-      document.querySelector("#todos").appendChild(listItem);
-      document.querySelector("#error").className = "hidden";
+document.querySelector('#todo-form').onsubmit = event => {
+    event.preventDefault();
+    fetch('/todos/create', {
+        method: 'POST',
+        body: JSON.stringify({
+            description: document.querySelector('[name="description"]').value,
+            list_id: document.querySelector('#list-select').value,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
-    .catch(() => {
-      document.querySelector("#error").className = "";
-    });
-  document.querySelector('[name="description"]').value = "";
+        .then(response => response.json())
+        .then(jsonResponse => {
+            console.log(jsonResponse);
+            const listItem = document.createElement('li');
+            listItem.innerHTML = jsonResponse['description'];
+            document.querySelector('#todos').appendChild(listItem);
+            document.querySelector('#error').className = 'hidden';
+        })
+        .catch(() => {
+            document.querySelector('#error').className = '';
+        });
+    document.querySelector('[name="description"]').value = '';
 };
